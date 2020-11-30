5304e61bdb42790415831680806afab88fc64405
s.kogawa <s.kogawa@sbfw.co.jp>
2020-11-30 10:23:11 +0900

    テストクラスを作成 テスト内容は未実装

---
+++ src/test/java/com/sample/GenericObjectPoolTest.java
@@ -0,0 +1,22 @@
+package com.sample;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GenericObjectPoolTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testBorrowObject() {
+		//fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testReturnObject() {
+		//fail("まだ実装されていません");
+	}
+
+}
